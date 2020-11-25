87d5ae3998c0a2608b8ae42c40e525b652abb592
RafaelResende <rafaelchina@gmail.com>
2020-11-25 02:02:22 -0300

    Add files via upload
    listar e inserir finalizado

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ Flask app
 """
 
 import os
+import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from collections import defaultdict
 from db import *
 from modelos import *
 app = Flask(__name__)
+
 
 __author__ = "Rafael Resende"
 __copyright__ = "Copyright 2020"
@@ -33,7 +35,6 @@ def listar_imobiliaria(imob_id):
     imobiliaria = Imobiliaria.query.get(imob_id)
     if imobiliaria is not None:
         return imobiliaria.to_obj()
-    return ""
 
 @app.route('/imobiliaria/inserir', methods=['POST'])
 def inserir_imobiliaria():
@@ -42,13 +43,8 @@ def inserir_imobiliaria():
         imob = Imobiliaria(data['nome'], data['endereco'])
         db_session.add(imob)
         db_session.commit()
+        return "ok"
 
-@app.route('/imobiliaria/selecionar', methods=['GET'])
-def selecionar_imobiliaria():
-    data = request.form
-    if data:
-        imob = User.query.filter_by(username=username).all()
-        return imob
 
 @app.route('/imobiliaria/editar', methods=['POST'])
 def editarr_imobiliaria():
@@ -58,22 +54,42 @@ def editarr_imobiliaria():
         db_session.add(imob)
         db_session.commit()
 
-@app.route('/imobiliaria/deletar', methods=['POST'])
+@app.route('/imobiliaria/deletar', methods=['DELETE'])
 def deletar_imobiliaria():
     data = request.form
     if data:
         imob = Imobiliaria(data['nome'], data['endereco'])
+        print (imob)
         db_session.delete(imob)
         db_session.commit()
+        return "delOK"
+
 
 #crud imovel
+
+@app.route('/imovel/listar')
+def listar_imovels():
+    imovels = Imovel.query.all()
+    imovels = [x.to_obj() for x in imovels]
+    return jsonify(imovels=imovels)
+
+
+@app.route('/imovel/listar/<int:imob_id>')
+def listar_imovel(imob_id):
+    imovel = Imovel.query.get(imob_id)
+    if imovel is not None:
+        return imovel.to_obj()
+
 @app.route('/imovel/inserir', methods=['POST'])
 def inserir_imovel():
+    print('oi1')
     data = request.form
+    print('oi2')
     if data:
-        imob = Imovel(data['nome'], data['endereco'])
-        db_session.add(imob)
+        imov = Imovel(data['nome'], data['endereco'], data['descricao'], data['status'], data['caracteristica'], data['tipo'], data['finalidade'], data['imobiliaria'])
+        db_session.add(imov)
         db_session.commit()
+        return "ok"
 
 
 @app.route('/imovel/selecionar', methods=['GET'])
